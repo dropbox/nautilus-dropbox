1e34db8cc9e8b5fad1c506dd2bd05356c20471e1
Rian Hunter <rian@getdropbox.com>
2008-07-03 16:03:36 -0700

    release preparation

--- src/nautilus-dropbox.h
+++ src/nautilus-dropbox.h
@@ -1,6 +1,22 @@
 /*
- *  nautilus-dropbox.h
- * 
+ * nautilus-dropbox.h
+ * Header file for nautilus-dropbox.c
+ *
+ * This file is part of nautilus-dropbox.
+ *
+ * nautilus-dropbox is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * nautilus-dropbox is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with nautilus-dropbox.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 #ifndef NAUTILUS_DROPBOX_H
@@ -71,15 +87,14 @@ struct _NautilusDropboxClass {
 	GObjectClass parent_slot;
 };
 
-GType nautilus_dropbox_get_type      (void);
-void  nautilus_dropbox_register_type (GTypeModule *module);
+GType nautilus_dropbox_get_type(void);
+void  nautilus_dropbox_register_type(GTypeModule *module);
 
 extern gboolean dropbox_use_nautilus_submenu_workaround;
 
 void nautilus_dropbox_on_connect(NautilusDropbox *cvs);
 
 void nautilus_dropbox_on_disconnect(NautilusDropbox *cvs);
-
 
 G_END_DECLS
 
