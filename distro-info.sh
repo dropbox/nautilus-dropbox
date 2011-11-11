defa060fee5f570099a68a3290eb242a7fcf0b08
David Euresti <david@dropbox.com>
2011-11-11 13:15:53 -0800

    Delete bad and old versions of the server when installing. Also Fixes #2412 and beyond.

--- distro-info.sh
+++ distro-info.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # This file also gets execfile'd by python so don't do anything here besides set variables.
-UBUNTU_CODENAMES="hardy intrepid jaunty karmic lucid maverick natty oneiric"
+UBUNTU_CODENAMES="hardy intrepid jaunty karmic lucid maverick natty oneiric precise"
 DEBIAN_CODENAMES="squeeze wheezy sid"
-FEDORA_CODENAMES="9 10 11 12 13 14 15"
+FEDORA_CODENAMES="9 10 11 12 13 14 15 16 17"
