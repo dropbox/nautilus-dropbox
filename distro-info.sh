b5a9f020ce436b7967ff2d8f9325182d8270e688
David Euresti <david@dropbox.com>
2013-02-22 17:11:23 -0800

    Add quantal 12.10.

--- distro-info.sh
+++ distro-info.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # This file also gets execfile'd by python so don't do anything here besides set variables.
-UBUNTU_CODENAMES="hardy intrepid jaunty karmic lucid maverick natty oneiric precise"
+UBUNTU_CODENAMES="hardy intrepid jaunty karmic lucid maverick natty oneiric precise quantal"
 DEBIAN_CODENAMES="squeeze wheezy sid"
 FEDORA_CODENAMES="9 10 11 12 13 14 15 16 17 18"
