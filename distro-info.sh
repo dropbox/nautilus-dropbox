de7b58e7189ace9e36a4940d3240148118104764
Luke Faraone <luke@faraone.cc>
2017-05-22 16:36:10 +0200

    Add Ubuntu y, z, a; current Debian testing

--- distro-info.sh
+++ distro-info.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # This file also gets execfile'd by python so don't do anything here besides set variables.
-UBUNTU_CODENAMES="lucid maverick natty oneiric precise quantal raring saucy trusty utopic vivid wily xenial"
+UBUNTU_CODENAMES="lucid maverick natty oneiric precise quantal raring saucy trusty utopic vivid wily xenial yakkety zesty artful"
 DEBIAN_CODENAMES="squeeze wheezy jessie sid"
 FEDORA_CODENAMES="9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24"
