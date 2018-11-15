cdc42c4352ebf095adc5f4cc4dbddd8b2fb11f97
Samer Masterson <samer@samertm.com>
2018-11-14 16:28:25 -0800

    Add "buster" to debian codenames we distribute to

--- distro-info.sh
+++ distro-info.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # This file also gets execfile'd by python so don't do anything here besides set variables.
 UBUNTU_CODENAMES="trusty utopic vivid wily xenial yakkety zesty artful bionic cosmic"
-DEBIAN_CODENAMES="jessie stretch sid"
+DEBIAN_CODENAMES="jessie stretch buster sid"
 FEDORA_CODENAMES="21 22 23 24 25 26 27 28 29"
