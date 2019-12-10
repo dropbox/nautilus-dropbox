3e482445cebd75a280f097656f175b59499ae77c
Vinson Lee <vlee@freedesktop.org>
2019-12-09 23:23:01 -0800

    Add Fedora 30.
    Signed-off-by: Vinson Lee <vlee@freedesktop.org>

--- distro-info.sh
+++ distro-info.sh
@@ -2,4 +2,4 @@
 # This file also gets execfile'd by python so don't do anything here besides set variables.
 UBUNTU_CODENAMES="trusty utopic vivid wily xenial yakkety zesty artful bionic cosmic disco"
 DEBIAN_CODENAMES="jessie stretch buster sid"
-FEDORA_CODENAMES="21 22 23 24 25 26 27 28 29"
+FEDORA_CODENAMES="21 22 23 24 25 26 27 28 29 30"
