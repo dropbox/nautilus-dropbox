f25922db6f432eb8e949c30bb37672b209f421ba
Timothy Palpant <palpant@dropbox.com>
2023-03-15 18:31:32 +0000

    Add Fedora 39 to distro-info.sh

--- distro-info.sh
+++ distro-info.sh
@@ -2,4 +2,4 @@
 # This file also gets execfile'd by python so don't do anything here besides set variables.
 UBUNTU_CODENAMES="kinetic lunar"
 DEBIAN_CODENAMES="bookworm sid"
-FEDORA_CODENAMES="37 38"
+FEDORA_CODENAMES="37 38 39"
