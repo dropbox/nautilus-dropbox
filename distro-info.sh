c99447bb18b6f9f85b08d4ff5eec9dd605af8f8c
Julia Pitts <jpitts@dropbox.com>
2024-01-19 10:56:43 -0800

    Update distro-info.sh to include new Ubuntu 24.04, Debian Trixie, and Fedora 40. (#133)

--- distro-info.sh
+++ distro-info.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # This file also gets execfile'd by python so don't do anything here besides set variables.
-UBUNTU_CODENAMES="kinetic lunar mantic"
-DEBIAN_CODENAMES="bookworm sid"
-FEDORA_CODENAMES="37 38 39"
+UBUNTU_CODENAMES="kinetic lunar mantic noble"
+DEBIAN_CODENAMES="bookworm trixie sid"
+FEDORA_CODENAMES="37 38 39 40"
