3dd636505c0b57d489d5787334e83d5ab9d783bb
Julia Pitts <jpitts@dropbox.com>
2024-01-19 18:15:04 +0000

    Add Debian Trixie and Fedora 40 to supported distros.

--- install-apt-deps.sh
+++ install-apt-deps.sh
@@ -2,4 +2,4 @@
 
 apt-get update
 apt-get install -y gnome-common libgtk-4-dev libnautilus-extension-dev python3-gi python3-docutils \
-    debootstrap devscripts libnautilus-extension-dev cdbs debian-archive-keyring
+    debootstrap devscripts libnautilus-extension-dev cdbs debian-archive-keyring debhelper
