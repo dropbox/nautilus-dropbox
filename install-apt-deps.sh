78aa7fae4914d8b29dbe1aa18e5f485fc68155e8
Manvi Nagdev <manvi@dropbox.com>
2025-04-22 19:13:55 -0400

    attempt to resolve missing package

--- install-apt-deps.sh
+++ install-apt-deps.sh
@@ -3,5 +3,4 @@
 apt-get update
 apt-get install -y gnome-common libgtk-4-dev libnautilus-extension-dev python3-gi python3-docutils \
     debootstrap devscripts libnautilus-extension-dev cdbs debian-archive-keyring debhelper \
-    gir1.2-gdkpixbuf-2.0 python3-gi-cairo libgdk-pixbuf2.0-dev libgdk-pixbuf2.0-0 \
-    gir1.2-gtk-3.0 gir1.2-glib-2.0 python3-cairo
+    libgdk-pixbuf-2.0-0
