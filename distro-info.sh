75bfafb3ac4baf7c3e756600db060d83687a9436
Manvi Nagdev <manvi@dropbox.com>
2025-04-22 16:31:59 -0400

    Revert "Add Fedora 42 and bump version to 2025.04.22"
    This reverts commit 37925730fc1af9ef7ce338b2b10714602b0a9d70.

--- distro-info.sh
+++ distro-info.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # This file also gets execfile'd by python so don't do anything here besides set variables.
-UBUNTU_CODENAMES="kinetic lunar mantic noble oracular plucky"
+UBUNTU_CODENAMES="kinetic lunar mantic noble oracular"
 DEBIAN_CODENAMES="bookworm trixie sid"
-FEDORA_CODENAMES="37 38 39 40 41 42"
+FEDORA_CODENAMES="37 38 39 40 41"
