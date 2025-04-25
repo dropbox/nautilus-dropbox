4caf7a810fe57c47db2eaeda0433f6e7e3fa68d0
Manvi Nagdev <manvi@dropbox.com>
2025-04-25 12:40:50 -0400

    update ubuntu name

--- install-rpm-deps.sh
+++ install-rpm-deps.sh
@@ -2,4 +2,4 @@
 
 dnf install -y gnome-common nautilus-devel gtk4-devel python3-docutils python3-gobject \
     devscripts mock rpm rpm-sign rpmdevtools rpmlint expect createrepo cdbs gnome-common \
-    python3-docutils rpm-build gawk which atk-devel gdk-pixbuf2
+    python3-docutils rpm-build gawk which atk-devel
