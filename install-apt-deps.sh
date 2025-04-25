4ca1393a38bb073fffea3c384ce7c35a7f95af1f
Manvi Nagdev <manvi@dropbox.com>
2025-04-25 11:21:35 -0400

    fix package name

--- install-apt-deps.sh
+++ install-apt-deps.sh
@@ -3,5 +3,5 @@
 apt-get update
 apt-get install -y gnome-common libgtk-4-dev libnautilus-extension-dev python3-gi python3-docutils \
     debootstrap devscripts libnautilus-extension-dev cdbs debian-archive-keyring debhelper \
-    libgdk-pixbuf-2.0-0 gir1.2-gdkpixbuf-2.0 libgdk-pixbuf2.0-dev \
-    python3-gi-cairo python3-cairo gir1.2-gtk-3.0 gir1.2-glib-2.0
+    libgdk-pixbuf-2.0-0 gir1.2-gdkpixbuf-2.0 libgdk-pixbuf-xlib-2.0-dev python3-gi-cairo \
+    python3-cairo gir1.2-gtk-3.0 gir1.2-glib-2.0
