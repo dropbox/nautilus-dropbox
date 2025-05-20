085b5551e533f04bbe52a355d773a144b71e7e07
Manvi Nagdev <manvi@dropbox.com>
2025-05-20 13:09:48 -0400

    changed to libgdk-pixbuf-xlib-2.0-dev

--- install-apt-deps.sh
+++ install-apt-deps.sh
@@ -4,4 +4,4 @@ apt-get update
 apt-get install -y gnome-common libgtk-4-dev libnautilus-extension-dev python3-gi python3-docutils \
     debootstrap devscripts libnautilus-extension-dev cdbs debian-archive-keyring debhelper \
     gdk-pixbuf-2.0 gir1.2-gdkpixbuf-2.0 python3-gi-cairo gir1.2-gtk-3.0 \
-    libgdk-pixbuf2.0-dev libglib2.0-dev
+    libgdk-pixbuf-xlib-2.0-dev libglib2.0-dev
