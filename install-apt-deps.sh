f528d52ab8758bf30943b1a56d8033b2cf9bcf75
Manvi Nagdev <manvi@dropbox.com>
2025-05-22 12:51:31 -0400

    update dependencies

--- install-apt-deps.sh
+++ install-apt-deps.sh
@@ -2,6 +2,4 @@
 
 apt-get update
 apt-get install -y gnome-common libgtk-4-dev libnautilus-extension-dev python3-gi python3-docutils \
-    debootstrap devscripts libnautilus-extension-dev cdbs debian-archive-keyring debhelper \
-    gdk-pixbuf-2.0 gir1.2-gdkpixbuf-2.0 python3-gi-cairo gir1.2-gtk-4.0 \
-    libgdk-pixbuf-xlib-2.0-dev libglib2.0-dev libgtk-4-0
+    debootstrap devscripts libnautilus-extension-dev cdbs debian-archive-keyring debhelper
