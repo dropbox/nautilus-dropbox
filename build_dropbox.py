b33122d5e9a799b9f9087f2fffb52dfa201ba81f
Manvi Nagdev <manvi@dropbox.com>
2025-05-22 12:49:08 -0400

    changed to gtk version 4

--- build_dropbox.py
+++ build_dropbox.py
@@ -5,8 +5,8 @@ import gi
 # Set GDK_BACKEND to offscreen before importing GTK
 os.environ['GDK_BACKEND'] = 'offscreen'
 
-gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk, GdkPixbuf
+gi.require_version('Gtk', '4.0')
+from gi.repository import Gtk, Gdk
 
 import re
 
@@ -22,17 +22,15 @@ if __name__ == '__main__':
     Gtk.init()
     
     # Load images using GTK
-    pixbuf64 = GdkPixbuf.Pixbuf.new_from_file("data/icons/hicolor/64x64/apps/dropbox.png")
-    pixbuf16 = GdkPixbuf.Pixbuf.new_from_file("data/icons/hicolor/16x16/apps/dropbox.png")
+    pixbuf64 = Gdk.Texture.new_from_filename("data/icons/hicolor/64x64/apps/dropbox.png")
+    pixbuf16 = Gdk.Texture.new_from_filename("data/icons/hicolor/16x16/apps/dropbox.png")
     
     src2dest = {'@PACKAGE_VERSION@': sys.argv[1],
                 '@DESKTOP_FILE_DIR@': sys.argv[2],
-                '@IMAGEDATA64@': ("GdkPixbuf.Pixbuf.new_from_data(%r, GdkPixbuf.Colorspace.RGB, %r, %r, %r, %r, %r)" %
-                                  (pixbuf64.get_pixels(), pixbuf64.get_has_alpha(), pixbuf64.get_bits_per_sample(),
-                                   pixbuf64.get_width(), pixbuf64.get_height(), pixbuf64.get_rowstride())),
-                '@IMAGEDATA16@': ("GdkPixbuf.Pixbuf.new_from_data(%r, GdkPixbuf.Colorspace.RGB, %r, %r, %r, %r, %r)" %
-                                  (pixbuf16.get_pixels(), pixbuf16.get_has_alpha(), pixbuf16.get_bits_per_sample(),
-                                   pixbuf16.get_width(), pixbuf16.get_height(), pixbuf16.get_rowstride())),
+                '@IMAGEDATA64@': ("Gdk.Texture.new_from_pixels(%r, %r, %r, %r)" %
+                                  (pixbuf64.get_pixels(), pixbuf64.get_width(), pixbuf64.get_height(), pixbuf64.get_format())),
+                '@IMAGEDATA16@': ("Gdk.Texture.new_from_pixels(%r, %r, %r, %r)" %
+                                  (pixbuf16.get_pixels(), pixbuf16.get_width(), pixbuf16.get_height(), pixbuf16.get_format())),
                 }
 
     buf = sys.stdin.read()
--- install-apt-deps.sh
+++ install-apt-deps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y gnome-common libgtk-3-dev libnautilus-extension-dev python3-gi python3-docutils \
+apt-get install -y gnome-common libgtk-4-dev libnautilus-extension-dev python3-gi python3-docutils \
     debootstrap devscripts libnautilus-extension-dev cdbs debian-archive-keyring debhelper \
-    gdk-pixbuf-2.0 gir1.2-gdkpixbuf-2.0 python3-gi-cairo gir1.2-gtk-3.0 \
-    libgdk-pixbuf-xlib-2.0-dev libglib2.0-dev libgtk-3-0
+    gdk-pixbuf-2.0 gir1.2-gdkpixbuf-2.0 python3-gi-cairo gir1.2-gtk-4.0 \
+    libgdk-pixbuf-xlib-2.0-dev libglib2.0-dev libgtk-4-0
