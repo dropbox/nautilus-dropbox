79a40336606688ef42bbe071989b85fed2c57106
Julia Pitts <jpitts@dropbox.com>
2026-01-15 21:42:32 +0000

    Add Debian 14 Forky.

--- distro-info.sh
+++ distro-info.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # This file also gets execfile'd by python so don't do anything here besides set variables.
 UBUNTU_CODENAMES="kinetic lunar mantic noble oracular plucky questing resolute"
-DEBIAN_CODENAMES="bookworm trixie sid"
+DEBIAN_CODENAMES="bookworm trixie forky sid"
 FEDORA_CODENAMES="37 38 39 40 41 42 43"
