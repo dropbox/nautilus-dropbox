669f65eda1bab36a233be387d2d9b3a12a221afb
Julia Pitts <jpitts@dropbox.com>
2026-01-15 14:00:06 -0800

    Fix CI. (#162)
    Updating the `dnf` flags and adding new distro versions.

--- install-rpm-deps.sh
+++ install-rpm-deps.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-dnf install -y gnome-common nautilus-devel gtk4-devel python3-docutils python3-gobject \
-    devscripts mock rpm rpm-sign rpmdevtools rpmlint expect createrepo cdbs gnome-common \
-    python3-docutils rpm-build gawk which atk-devel
+dnf install -y --skip-unavailable gnome-common nautilus-devel gtk4-devel python3-docutils \
+    python3-gobject devscripts mock rpm rpm-sign rpmdevtools rpmlint expect createrepo cdbs \
+    gnome-common python3-docutils rpm-build gawk which atk-devel
